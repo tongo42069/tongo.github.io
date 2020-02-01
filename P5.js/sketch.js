efba75671d3611730d86e03f0728648d16da8c04
Tong <tong.pnk@gmail.com>
2020-02-01 10:05:12 +0100

    Prøvede og opload en hhjemmeside

---
+++ P5.js/sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+  createCanvas(400, 400);
+  frameRate(2);
+}
+
+function draw() {
+  background(220);
+}
+function draw() {
+	ellipse(250, 50, 100, 100);
+  	line(250, 100, 250, 300);
+line(250, 300, 350, 400);
+  line(250, 300, 150, 400)
+ellipse(225, 20, 30, 25);
+  
+  
+  if(frameCount%4 == 0) 
+		line(250, 150, 350, -30);
+		line(250, 150, 350, -50);
+  
+}
+	
